106b5151946cfef6c6f3828433f54a32cef8d0ee
Dana-Kaziyeva <dana.kaazieva@gmail.com>
2023-10-15 13:36:00 +0600

    Creation of a Welcome page

---
+++ app/src/main/java/com/example/loginlayoutpage/pages/WelcomePage.kt
@@ -0,0 +1,19 @@
+package com.example.loginlayoutpage.pages
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WelcomePage(
+    modifier: Modifier = Modifier
+){
+    Surface (
+        modifier = modifier.fillMaxSize()
+    ){
+
+    }
+
+
+}
