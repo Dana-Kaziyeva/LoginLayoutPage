a2b0791016802b4b3f5cd89047bebf8c57caf11d
Dana-Kaziyeva <dana.kaazieva@gmail.com>
2023-10-15 17:12:47 +0600

    Welcome page 1.3

--- app/src/main/java/com/example/loginlayoutpage/pages/WelcomePage.kt
+++ app/src/main/java/com/example/loginlayoutpage/pages/WelcomePage.kt
@@ -2,45 +2,38 @@ package com.example.loginlayoutpage.pages
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.loginlayoutpage.R
+import com.example.loginlayoutpage.ui.theme.BlueMain
 import com.example.loginlayoutpage.ui.theme.LoginLayoutPageTheme
+import com.example.loginlayoutpage.ui.theme.MainBackgroundColor
+import com.example.loginlayoutpage.ui.theme.YellowMain
 
 @Composable
 fun WelcomePage(
     modifier: Modifier = Modifier
 ){
     Surface(modifier = modifier.fillMaxSize(),
-        color = Color(238, 200, 164, 255)
+        color = MainBackgroundColor
     ){
         Column(modifier = Modifier
             .padding(15.dp)
@@ -66,8 +59,8 @@ fun WelcomePage(
                     modifier = Modifier
                         .padding(bottom = 20.dp, end = 40.dp, start = 40.dp)
                         .width(300.dp),
-                    colors = buttonColors(containerColor = Color(0,113,188)),
-                    border = BorderStroke(3.dp, Color(247,178,59)),
+                    colors = buttonColors(containerColor = BlueMain),
+                    border = BorderStroke(3.dp, YellowMain),
                 )
                 {
                     Text(text = stringResource(R.string.register), fontSize = 30.sp)
@@ -78,8 +71,8 @@ fun WelcomePage(
                     modifier = Modifier
                         .padding(top = 20.dp, end = 40.dp, start = 40.dp)
                         .width(300.dp),
-                    colors = buttonColors(containerColor = Color(0,113,188)),
-                    border = BorderStroke(3.dp, Color(247,178,59)),
+                    colors = buttonColors(containerColor = BlueMain),
+                    border = BorderStroke(3.dp, YellowMain),
                 )
                 {
                     Text(text = stringResource(R.string.log_in), fontSize = 30.sp)
--- app/src/main/java/com/example/loginlayoutpage/ui/theme/Color.kt
+++ app/src/main/java/com/example/loginlayoutpage/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val MainBackgroundColor = Color(238, 200, 164, 255)
+val BlueMain = Color(0,113,188)
+val YellowMain = Color(247,178,59)
