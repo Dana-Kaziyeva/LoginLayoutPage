3fd73d29ed12fc3e45fda3cf92727d2745c87c3b
Dana-Kaziyeva <dana.kaazieva@gmail.com>
2023-10-15 18:22:02 +0600

    Home page UI

---
+++ app/src/main/java/com/example/loginlayoutpage/pages/Home Page.kt
@@ -0,0 +1,41 @@
+package com.example.loginlayoutpage.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loginlayoutpage.ui.theme.BlueMain
+import com.example.loginlayoutpage.ui.theme.LoginLayoutPageTheme
+import com.example.loginlayoutpage.ui.theme.MainBackgroundColor
+
+@Composable
+fun HomePage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MainBackgroundColor)
+            .padding(50.dp),
+    ) {
+        Text(
+            text = "Hello world! It's home page!",
+            color = BlueMain,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePageAppPreview() {
+    LoginLayoutPageTheme {
+        HomePage()
+    }
+}
